3e57fa8939de811a70424c8749a180ca511add56
Pavel418 <pavel@vvv.az>
2022-09-08 22:52:00 +0400

    loading from database

--- PizzaWebsite/Pages/Menu.cshtml.cs
+++ PizzaWebsite/Pages/Menu.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaWebsite.Data;
+using PizzaWebsite.Services;
 
 namespace PizzaWebsite.Pages
 {
     public class MenuModel : PageModel
     {
-        public void OnGet()
+        public ICollection<Pizza>? Pizzas { get; set; }
+
+        GetPostService _service;
+
+        public MenuModel(GetPostService service)
         {
+            _service = service;
+        }
+
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Pizzas = await _service.GetPizzasAsync();
+
+            return Page();
         }
     }
 }

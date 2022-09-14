8867a09ca474da503f24b85ef854edc50ecb9f38
NedNedov228 <nakhmedli@gmail.com>
2022-09-15 00:51:45 +0300

    Adaptive Slider

--- PizzaWebsite/wwwroot/js/site.js
+++ PizzaWebsite/wwwroot/js/site.js
@@ -3,3 +3,58 @@
 
 // Write your JavaScript code.
 
+
+// ADAPTIVE SLDER
+
+const images = document.querySelectorAll(".slider-wrap .slider img");
+const slider = document.querySelector(".slider");
+
+let count = 0;
+let width;
+
+function resize() {
+    width = document.querySelector(".slider-wrap").offsetWidth;
+    slider.style.width = width * images.length + "px";
+
+    images.forEach(item => {
+        item.style.width = width + "px";
+        item.style.height = "auto";
+        rollSlider();
+    });
+}
+
+
+window.addEventListener("resize", resize);
+resize();
+
+document.querySelector(".slide-next").addEventListener("click", function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+
+});
+
+document.querySelector(".slide-prev").addEventListener("click", function () {
+    count--;
+    if (count < 0) {
+        count = images.length -1;
+    }
+    rollSlider();
+
+});
+
+
+function rollSlider() {
+
+    slider.style.transform = "translate(-" + count * width + "px)"; 
+}
+
+let timer = setInterval(function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+}, 5000);

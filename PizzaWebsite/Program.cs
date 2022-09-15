eff656509118673c5473af3d8622c8e80ff886b2
Pavel418 <pavel@vvv.az>
2022-09-15 22:08:24 +0400

    validation error

--- PizzaWebsite/Program.cs
+++ PizzaWebsite/Program.cs
@@ -44,16 +44,16 @@ builder.Services.AddAuthorization(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
 }
 else
-{
+{*/
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
